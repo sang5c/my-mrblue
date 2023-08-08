b4fb6855627075299227719e77ee64dbb8fd2cf5
sang5c <skah321@gmail.com>
2023-08-08 09:52:17 +0900

    init

---
+++ src/main/java/com/example/mymrblue/MyMrblueApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymrblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMrblueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMrblueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mymrblue/MyMrblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymrblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMrblueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
